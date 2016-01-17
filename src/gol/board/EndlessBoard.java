37a847242ff5b4427130b57c183c6880fef90250
Tino Steinort <tinosteinort@live.de>
2016-01-17 17:45:23 +0100

    Add Board Skeletons

---
+++ src/gol/board/EndlessBoard.java
@@ -0,0 +1,7 @@
+package gol.board;
+
+/**
+ * Created by Tino on 17.01.2016.
+ */
+public class EndlessBoard extends Board {
+}
---
+++ src/gol/board/TorusBoard.java
@@ -0,0 +1,11 @@
+package gol.board;
+
+/**
+ * Created by Tino on 17.01.2016.
+ */
+public class TorusBoard extends BoundedBoard {
+
+    public TorusBoard(final int width, final int height) {
+        super(width, height);
+    }
+}
